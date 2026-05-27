f3ef8a447cb042df87f331b5b340022888a4fdb6
Sameer <sameerahire674@gmail.com>
2026-05-28 01:04:44 +0530

    @Annotation Based Example and its information ex. @component,@configuration,@reporitory,@componentScan,@Controller,etc type annotation is used.

---
+++ SpringMicroservices-4/src/main/java/com/sam/config.java
@@ -0,0 +1,10 @@
+package com.sam;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration //above annotation will provide default annotation.if you want to print the info from another class then we use.
+@ComponentScan //when you declare the component annotation then this annotation is used to fetch the information.
+public class config {
+
+}
