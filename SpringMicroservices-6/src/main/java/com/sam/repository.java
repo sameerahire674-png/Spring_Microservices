d29e80eac85547c482e09f82364e221b6b9339cb
Sameer <sameerahire674@gmail.com>
2026-05-29 01:33:26 +0530

    @Anotation based information and also uses in normal Program

---
+++ SpringMicroservices-6/src/main/java/com/sam/repository.java
@@ -0,0 +1,11 @@
+package com.sam;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class repository {
+	public repository() {
+		System.out.println("This @ Repository anntation is used for the (DataBase Logic class) EX- DAO Class");
+	}
+
+}
